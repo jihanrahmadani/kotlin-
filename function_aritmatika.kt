825c5a0ca69b06c56ba639a716edfc353b9f8ad0
jihanrahmadani <156177382+jihanrahmadani@users.noreply.github.com>
2024-01-20 09:54:31 +0700

    Create function_aritmatika.kt

---
+++ function_aritmatika.kt
@@ -0,0 +1,43 @@
+fun jumlah(a: Int, b: Int): Int{
+    return a + b 
+}
+
+fun kurang(a: Int, b: Int): Int{
+    return a - b
+}
+fun kali(a: Int, b: Int): Int{
+    return a * b
+}
+
+fun bagi(a: Double, b: Double): Double{
+    return a / b
+}
+
+
+fun segitiga(sgt: Double, a: Double, t: Double): Double{
+    return 0.5 * a * t
+}
+
+fun persegipnjng(p: Int, l: Int): Int{
+    return p * l
+}
+
+fun main() {
+    val add = jumlah (10, 23)
+    println("ini adalah hasil penjumlahan: $add")
+   
+   val min = kurang (23, 10)
+   println("ini adalah hasil pengurangan: $min")
+   
+   val mul = kali (23, 10)
+   println("ini adalah hasil perkalian: $mul")
+   
+    val dif = bagi (23.0, 10.0)
+   println("ini adalah hasil pembagian: $dif")
+   
+   val pp = persegipnjng(20, 10)
+   println("ini adalah hasil luas persegi panjang: $pp")
+   
+   val sgt = segitiga(0.5, 20.0, 10.0)
+   println("ini adalah hasil luas segitiga: $sgt")
+}
